a4ce05bdd07f758f613953bc4cf37183dd9eb3d9
Britney Wright <blw06g@gmail.com>
2014-09-15 18:28:26 -0400

    generate spree installation

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,3 +1,7 @@
 # Load BrowserCMS seed data
 
 require File.expand_path('../browsercms.seeds.rb', __FILE__)
+
+
+Spree::Core::Engine.load_seed if defined?(Spree::Core)
+Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
