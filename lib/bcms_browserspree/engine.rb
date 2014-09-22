117d4f1623c7d8e51d3cd5b731269b3a8aabb040
Britney Wright <blw06g@gmail.com>
2014-09-22 19:26:29 -0400

    add recent products portlet

--- lib/bcms_browserspree/engine.rb
+++ lib/bcms_browserspree/engine.rb
@@ -3,5 +3,7 @@ module BcmsBrowserspree
   class Engine < ::Rails::Engine
     isolate_namespace BcmsBrowserspree
 		include Cms::Module
+
+    config.autoload_paths += %W(#{config.root}/app/portlets/helpers)
   end
 end
