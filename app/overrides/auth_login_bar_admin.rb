ca820f1adf7af9e8e3b04b0913712b0098722fb2
Britney Wright <blw06g@gmail.com>
2014-09-23 14:13:55 -0400

    modify auth_login_bar_admin.rb

--- app/overrides/auth_login_bar_admin.rb
+++ app/overrides/auth_login_bar_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_header",
  :name => "auth_login_bar_admin",
  :insert_after => "figure",
- :text => "<ul style='list-style: none; text-align: right;'><%= render :partial => 'spree/shared/login_bar' %></ul></nav>"
+ :partial => "spree/shared/admin_login_navigation_bar",
  )
